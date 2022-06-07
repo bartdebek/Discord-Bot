203c632a73c10594a346207a5081be8c51191b92
debus3 <debus3@gmail.com>
2022-06-07 21:03:56 +0200

    first commit

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# bot.py
 from multiprocessing.connection import Client
 import os
 import random
@@ -8,11 +7,9 @@ import asyncio
 from discord.ext import commands
 from dotenv import load_dotenv
 
-
-
 # Open a xls file with quotes and ads them to the list that is used by a bot
 
-df = pd.read_excel("Discord bot\cytaty.xls") 
+df = pd.read_excel("cytaty.xls") 
 mylist = df['Listacytatow'].tolist()
 
 
@@ -20,11 +17,7 @@ mylist = df['Listacytatow'].tolist()
 
 quizContestants = {} 
 
-def question_draw():
-    """Function that draws a quiz question number to be asked when used with !quiz command"""
 
-    question_number = random.randint(0,len(questionsList)-1)
-    return question_number
 
 
 # Assign Discord token kept in .env file
@@ -33,10 +26,6 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN') 
 
 # Open a file with quiz questions
-
-f = open("questionsJson3.json") 
-questionsList = json.load(f)
-questionsList = questionsList["trivia_questions"]
     
 # Assign "!" as a prefix to Discord commands
 client = commands.Bot(command_prefix='!')
@@ -53,17 +42,51 @@ async def janusz(ctx):
 
 # Commands to send a random question to the person that send "!quiz" message.
 
-@client.command(name='kwiz', help='Shows a general knowledge question. You can receive points for guessing correctly!')
+quizContestants = {}
+
+f = open("questionList.json") 
+questionsList = json.load(f)
+questionsList = questionsList["trivia_questions"]
+
+def question_draw():
+    """Function that draws a quiz question number to be asked when used with !quiz command"""
+
+    question_number = random.randint(0,len(questionsList)-1)
+    right_answer = questionsList[question_number]["right_answer"]
+
+    return question_number,right_answer
+
+
+
+class Question():
+    
+    def __init__(self,question_number,right_answer) -> None:
+        self.question_number = question_number
+        self.right_answer = right_answer
+
+    def __str__(self):
+        return (f"Question: {questionsList[self.question_number]['question']}\n\n"
+f"1. {questionsList[self.question_number]['answers'][0]}\n"
+f"2. {questionsList[self.question_number]['answers'][1]}\n"
+f"3. {questionsList[self.question_number]['answers'][2]}\n"
+f"4. {questionsList[self.question_number]['answers'][3]}\n")
+
+    def get_right_answer(self):
+        return(f"{questionsList[self.question_number]['answers'][self.right_answer-1]}")
+
+
+
+
+@client.command(name='quiz', help='Shows a general knowledge question. You can receive points for guessing correctly!')
 async def on_message(message):
     channel = message.channel
 
 # Drawing and sending the question as a discord message
 
-    QuestNumber = question_draw()
-    await channel.send("Quiz Starting...")
-    await channel.send(questionsList[QuestNumber]["question"])
-    await channel.send(f'\n1. {questionsList[QuestNumber]["answers"][0]}\n2. {questionsList[QuestNumber]["answers"][1]} \n3. {questionsList[QuestNumber]["answers"][2]} \n4. {questionsList[QuestNumber]["answers"][3]}')
-    rightAnswer = questionsList[QuestNumber]["right_answer"]
+    random_question = question_draw()
+    chosen_question = Question(random_question[0],random_question[1])
+    await channel.send(chosen_question)
+    right_answer = random_question[1]
 
 # Checking if author of answer was the person who used "!quiz" command, 
 # setting the maximum annswer time to 60 seconds. 
@@ -75,33 +98,40 @@ async def on_message(message):
     except asyncio.TimeoutError:
         await message.channel.send('Time is up')
 
-# If answer is given, program checks if it is the same as correct answer, if yes points are awarded
-# accordingly. Contestants dictionary is also added to contestants.txt file to be used as a backup  
+# If answer is given, program checks if it is the same as correct answer, if yes points are awarded accordingly. 
 
-    if int(guess.content) == rightAnswer:
+    if int(guess.content) == right_answer:
         await message.channel.send('Correct answer')
         nick = str(message.author)
+        
         if nick not in quizContestants:
+
             quizContestants[nick] = 0
             quizContestants[nick] = quizContestants[nick]+1
+
         else:
+
             quizContestants[nick] = quizContestants[nick]+1
+
         newPoints = quizContestants[nick]
+
+# Contestants dictionary is also added to contestants.txt file to be used as a backup  
+
         with open("contestants.txt","w") as file:
             file.write(json.dumps(quizContestants))
-        pointsMessage = ("Now {} has {} points.".format(nick,newPoints))
+        pointsMessage = (f'Now {nick} has {newPoints} points.')
         await message.channel.send(pointsMessage)
 
     else:
-        await message.channel.send('Wrong answer, correct answer is {}'.format(questionsList[QuestNumber]["answers"][rightAnswer-1]))
-        print(rightAnswer)
+        await message.channel.send(f'Wrong answer, correct answer is {chosen_question.get_right_answer()}')
+        print(right_answer)
     
 # Command brings up a quiz leaderboard
 
 @client.command(name='leaderboard', help='Shows current quiz leaderboard')
 async def leaderboard(ctx):
     for key, val in quizContestants.items():
-        x = "{}  {}".format(key,val)
+        x = (f"{key}  {val}")
         await ctx.send(x)
 
 
